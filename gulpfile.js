5e850cb751c48e0b268e7f7084933303daf4e4fd
aevitas <m4rtijn@gmail.com>
2018-05-16 15:48:53 +0200

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+
+const project = ts.createProject('tsconfig.json');
+
+gulp.task('scripts', () => {
+  const result = project.src()
+  .pipe(project());
+  return result.js.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['scripts'], () => {
+  gulp.watch('src/**/*.ts', ['scripts']);
+});
+
+gulp.task('assets', function() {
+  return gulp.src(JSON_FILES)
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['watch', 'assets']);
